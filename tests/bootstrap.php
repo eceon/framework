c4e4ec3036cc29d2be9cb2a0f76351bbe878bad2
Ted van Diepen <t.v.diepen@mezio.nl>
2015-08-18 14:22:05 +0200

    added a test bootstrap.php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,19 @@
 <?php
-/**
- * @author Ted van Diepen
- */
-// TODO: check include path
+    /**
+     * @author Ted van Diepen
+     */
 
-// put your code here
-?>
+    $sourcePath = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src';
+
+    // add the src directory to the include path
+    set_include_path(get_include_path() . PATH_SEPARATOR . $sourcePath);
+
+    // include psr-4 loader
+    require_once('Eceon\\Autoload.php');
+    
+    // create loader 
+    $loader = new Eceon\Autoload();
+    $loader->addNamespace( 'Eceon\\', $sourcePath . DIRECTORY_SEPARATOR . 'Eceon' );
+    $loader->register();
+ 
+    echo "loaded bootstrap\n";
